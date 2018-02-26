326ab176fa4368f8160d82e364210deaaae45231
Trond Werner Hansen <trond@kite.media>
2018-02-26 15:56:02 -0500

    No fontWeight

--- src/calendar/day/basic/style.js
+++ src/calendar/day/basic/style.js
@@ -15,7 +15,6 @@ export default function styleConstructor(theme={}) {
       marginTop: 4,
       fontSize: appStyle.textDayFontSize,
       fontFamily: appStyle.textDayFontFamily,
-      fontWeight: '300',
       color: appStyle.dayTextColor,
       backgroundColor: 'rgba(255, 255, 255, 0)'
     },
--- src/calendar/day/period/style.js
+++ src/calendar/day/period/style.js
@@ -39,7 +39,6 @@ export default function styleConstructor(theme={}) {
       marginTop: 7,
       fontSize: appStyle.textDayFontSize,
       fontFamily: appStyle.textDayFontFamily,
-      fontWeight: '300',
       color: appStyle.dayTextColor || '#2d4150',
       backgroundColor: 'rgba(255, 255, 255, 0)'
     },
